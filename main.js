1cb07d66e1c6c5f05c81b202e348c06d45b14f98
Vibhaas <vibhaas@gmail.com>
2018-12-31 21:37:55 +0530

    Created electron version

---
+++ main.js
@@ -0,0 +1,65 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+
+// listen for app to be ready
+app.on('ready', function(){
+  // create new window
+  mainWindow = new BrowserWindow({});
+  // load html file
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+  //Quit app when closed
+  mainWindow.on('closed', function (){
+    app.quit();
+  });
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: 'CmdOrCtrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  }
+];
+
+// If mac, add empty object to menuitem
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({
+    label: ''
+  });
+}
