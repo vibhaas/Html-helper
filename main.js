ddce11c87482bde1bfff87ab65af7c0a94008bb8
Vibhaas <vibhaas@gmail.com>
2019-04-22 07:45:59 +0530

    Mostly finished the save and open features

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const {app, BrowserWindow, Menu, ipcMain, dialog} = electron;
 
 let mainWindow;
 
+var openedFile = undefined;
+var changed = true;
+
 // listen for app to be ready
 app.on('ready', function(){
   // create new window
@@ -21,10 +24,14 @@ app.on('ready', function(){
     protocol: 'file',
     slashes: true
   }));
-  //Quit app when closed
+  // Quit app when closed
   mainWindow.on('closed', function (){
     app.quit();
   });
+  // Save function
+  function goSave() {
+    mainWindow.webContents.send('file:goSave', 'gosave');
+  }
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
@@ -37,22 +44,44 @@ function open(filePath) {
          console.log("An error ocurred reading the file :" + err.message);
          return;
      }
-
-     // Change how to handle the file content
-     console.log("The file content is : " + data);
+     // Check file type
+     if (path.extname(filePath) == ".html" || path.extname(filePath) == ".htm") {
+       // Send the code
+       mainWindow.webContents.send('file:open', data);
+       openedFile = filePath;
+       changed = false;
+    }
+    else {
+      console.log("File is not HTMl!");
+      return;
+    }
   });
 }
 
-function save(fileName) {
-  var content = ""; // Get this somehow [selectedTab.getContent()]
+function save(fileName, content) {
   fs.writeFile(fileName, content, (err) => {
       if(err){
           console.log("An error ocurred creating the file "+ err.message);
       }
-
+      openedFile = fileName;
+      changed = false;
       console.log("The file has been succesfully saved");
   });
 }
+
+// Catch Save (file:save)
+
+ipcMain.on('file:save', function(event, item) {
+  if(openedFile != undefined) {
+    save(openedFile, item);
+  }
+});
+
+// Catch Change (file:changed)
+
+ipcMain.on('file:changed', function(event, item) {
+  changed = item; // item will always be true here
+});
 
 // Create menu template
 const mainMenuTemplate = [
@@ -92,21 +121,38 @@ const mainMenuTemplate = [
         label: 'Save',
         accelerator: 'CmdOrCtrl+S',
         click(){
-          dialog.showSaveDialog((fileName) => {
-              if (fileName === undefined){
-                  console.log("An error occured");
-                  return;
-              }
-              // Access the save() function
-
-          });
+          if (openedFile == undefined) {
+            dialog.showSaveDialog((fileName) => {
+                if (fileName === undefined){
+                    console.log("An error occured");
+                    return;
+                }
+                openedFile = fileName;
+                goSave();
+            });
+          }
+          else {
+            goSave();
+          }
         }
       },
       {
         label: 'Save as',
         accelerator: 'CmdOrCtrl+Shift+S',
         click(){
-          console.log('Feature unavailable in this version.');
+          if (changed == true) {
+            // Unsaved handler
+          }
+          else {
+            dialog.showSaveDialog((fileName) => {
+                if (fileName === undefined){
+                    console.log("An error occured");
+                    return;
+                }
+                openedFile = fileName;
+                goSave();
+            });
+          }
         }
       },
       {
